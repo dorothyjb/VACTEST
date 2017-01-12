f1f3891a3a170e775b118f8adeb1888212b2df1e
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-01-12 16:37:15 -0500

    Initial create corrtyps migration.

---
+++ vahs/db/migrate/20170112000000_create_corrtyps.rb
@@ -0,0 +1,17 @@
+class CreateCorrTyps < ActiveRecord::Migration
+  def self.up
+    create_table "CORRTYPS", primary_key: "CTYPKEY", force: :cascade do |t|
+      t.string "CTYPVAL",  limit: 10
+      t.string "CTYADUSR", limit: 16
+      t.date   "CTYADTIM"
+      t.string "CTYMDUSR", limit: 16
+      t.date   "CTYMDTIM"
+      t.string "CTYACTVE", limit: 1
+      t.string "CTYSYS",   limit: 16
+    end
+  end
+
+  def self.down
+    drop_table "CORRTYPS"
+  end
+end
