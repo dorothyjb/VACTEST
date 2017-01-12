01ef97d7794e1318c4bd7ae4f7965bf0d4085431
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-01-12 16:57:34 -0500

    Initial RMDREA migration.

---
+++ vahs/db/migrate/20170112000000_create_rmdrea.rb
@@ -0,0 +1,16 @@
+class CreateRMDREA < ActiveRecord::Migration
+  def self.up
+   create_table "RMDREA", primary_key: "RMDKEY", force: :cascade do |t|
+      t.string  "RMDVAL",      limit: 2
+      t.string  "RMDMDUSR",    limit: 16
+      t.date    "RMDMDTIM"
+      t.string  "RMDPRIORITY", limit: 2
+      t.integer "RMDISSSEQ",   limit: 4
+      t.string  "RMDDEV",      limit: 2
+    end
+  end
+
+  def self.down
+    drop_table "RMDREA"
+  end
+end
