1fd1ef54b4ecad7f2ddab9b09500f3b145979d67
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-03-19 18:39:02 -0400

    Highlight links in rails instead of using jQuery document load.

--- vahs/app/helpers/application_helper.rb
+++ vahs/app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-	def title(page_title)
-	  content_for :title, page_title.to_s
-	end
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
+  def nav_class page
+    klass = "nav-item nav-link"
+    klass += " active" if current_page?(page)
+    klass
+  end
 end
