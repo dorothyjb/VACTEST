d73106baa50e5e9b9931fef6b1b9fd6c3a484018
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-03-22 22:37:27 -0400

    Initialize session

--- vahs/app/controllers/application_controller.rb
+++ vahs/app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_filter :init_session
+
+  def init_session
+    session[:docket_fiscal_years] ||=  [
+                                         [ "1970-09-30", "2000-09-30" ],
+                                         [ "2001-09-30", "2005-09-30" ],
+                                         [ "2006-09-30", "2010-09-30" ],
+                                         [ "2011-09-30", "2015-09-30" ],
+                                         [ "2016-09-30", "2017-09-30" ],
+                                         [ "2017-09-30", "2018-09-30" ],
+                                       ]
+  end
 end
