77d8506ddc99fe288fbac23ce1710c572f6e3359
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-03-24 07:39:31 -0400

    protect from forgery with exception instead of null_session; this was causing sessions to not work.

--- vahs/app/controllers/application_controller.rb
+++ vahs/app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   before_filter :init_session
 
   def init_session
+    #reset_session
     session[:docket_fiscal_years] ||=  [
                                          [ "1970-09-30", "2000-09-30" ],
                                          [ "2001-09-30", "2005-09-30" ],
