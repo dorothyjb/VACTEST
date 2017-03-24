057c40629ac44f3186f84a5507057ac0730d4502
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-03-24 00:09:00 -0400

    fiscalyear modifications

--- vahs/app/assets/javascripts/fiscalyear.js
+++ vahs/app/assets/javascripts/fiscalyear.js
@@ -1,30 +1,37 @@
 //= require jquery
 //= require jquery-ui
 
-//function addFiscalYear()
-//{
-	/*var fy_list = document.getElementByID('fiscal-years-list');
-	var rand_id = 1000 + Math.floor(Math.random() * 100);*/
-
-//	alert(fy_list);
-
-	/*fy_list.inner_html += '<div class="fiscal-year-entry">';
-	fy_list.inner_html += '<input type="text" name="fy_begin[]" id="" value="" alt="Begin Date" class="fy-date-entry">';
-	fy_list.inner_html += '<input type="text" name="fy_end[]" id="" value="" alt="End Date" class="fy-date-entry">';
-	fy_list.inner_html += '<input type="checkbox" name="fy[]" id="" value="' + rand_id + '" alt="Remove Fiscal Year range" class="fy-date-remove-checkbox">';
-	fy_list.inner_html += '</div>';*/
-//}
-
-function removeFiscalYear()
-{
-}
-
 $(document).ready(function()
 {
-	function addFiscalYear()
+	function enableDateEntry()
 	{
-		alert('Hello!');
+		$('.fy-date-entry').datepicker({
+			changeDay: true,
+			changeMonth: true,
+			changeYear: true,
+			showButtonPanel: true,
+			dateFormat: 'yy-mm-dd',
+			onClose: function(dateText, inst) {
+				$(this).datepicker('setDate', new Date(inst.selectedYear, inst.selectedMonth, inst.selectedDay)); 
+			}
+		});
 	}
 
+	function addFiscalYear()
+	{
+		var fy_list = document.getElementById('fiscal-years-list');
+		var rand_id = 1000 + Math.floor(Math.random() * 100);
+
+		fy_list.innerHTML += '<div class="fiscal-year-entry">\n';
+		fy_list.innerHTML += '  <input type="text" name="fy_begin[]" id="" value="" alt="Begin Date" class="fy-date-entry">\n';
+		fy_list.innerHTML += '  <input type="text" name="fy_end[]" id="" value="" alt="End Date" class="fy-date-entry">\n';
+		fy_list.innerHTML += '  <input type="checkbox" name="fy[]" id="" value="' + rand_id + '" alt="Remove Fiscal Year range" class="fy-date-remove-checkbox">\n';
+		fy_list.innerHTML += '</div>\n';
+
+		enableDateEntry();
+	}
+
+	
+	enableDateEntry();
 	$('#btnAddFY').on('click', addFiscalYear);
 });
