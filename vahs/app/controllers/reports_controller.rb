f81dcd8b5abafd93587b7042c0072c77b09ca736
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-03-30 10:50:09 -0400

    Totals for fiscal years.

--- vahs/app/controllers/reports_controller.rb
+++ vahs/app/controllers/reports_controller.rb
@@ -20,7 +20,7 @@ class ReportsController < ApplicationController
     # - bfDocDate: total of the records that are before Docket Date (bfDocDate)
     # - ttlPending: total of all returned records
     @ttls = {
-      'fyCol' => [0,0,0,0,0,0],
+      'fyCol' => nil,
       'bfDocDate' => 0, 
       'ttlPending' => 0
     }
@@ -29,16 +29,14 @@ class ReportsController < ApplicationController
     @output, @ttls["bfDocDate"], @ttls["ttlPending"] = Vacols::Brieff.get_report(@docdate, @hType, session[:docket_fiscal_years])
     @output = @output.sort_by { |h, obj| obj.total_pending }.sort_by { |h, obj| obj.docdate_total }.reverse
     @fiscal_years = @output.first[1].fiscal_years
+    @ttls['fyCol'] = Array.new(@fiscal_years.length, 0)
 
     #Parse the data to a JSON object and sum up the FY columns for the Totals row
-    #@output.each do |roName,obj|
-    #    @ttls["fyCol"][0] += obj.fiscal_years[0]
-    #    @ttls["fyCol"][1] += obj.fiscal_years[1]
-    #    @ttls["fyCol"][2] += obj.fiscal_years[2]
-    #    @ttls["fyCol"][3] += obj.fiscal_years[3]
-    #    @ttls["fyCol"][4] += obj.fiscal_years[4]
-    #    @ttls["fyCol"][5] += obj.fiscal_years[5]
-    #end
+    @output.each do |roName,obj|
+      obj.fiscal_years.each_with_index do |fy, i|
+        @ttls['fyCol'][i] += fy[:total]
+      end
+    end
 
     #Partials execute based on what 'exists'
     if params[:ViewResults]
@@ -49,9 +47,9 @@ class ReportsController < ApplicationController
         @exportXLS = JSON.parse(@output.to_json)
     end
     render :docket
-  #rescue Exception
-  #  @err = true
-  #  render :docket
+  rescue Exception
+    @err = true
+    render :docket
   end
 
   # Docket FY Analysis Reporting 
