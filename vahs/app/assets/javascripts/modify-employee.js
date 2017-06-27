c5d8f4db09c52bd03e0af2737ea22f20447058ef
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-06-27 14:33:18 -0400

    Change accordion from ID to class. Initial implementation of org code js management.

--- vahs/app/assets/javascripts/modify-employee.js
+++ vahs/app/assets/javascripts/modify-employee.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     });
   };
 
-  $('#accordion').togglepanels();
+  $('.accordion').togglepanels();
 
   $('#vet_status').change(function() {
     var str = $('#vet_status option:selected').text();
@@ -37,6 +37,55 @@ $(document).ready(function() {
       $('#military_svc_branch').removeClass('hide-element');
     } else {
       $('#military_svc_branch').addClass('hide-element');
+    }
+  });
+
+  $('#gp_field_office').change(function() {
+    $.ajax({
+      url: "/rms/orginization/office",
+      data: {
+        office: $(this).val()
+      },
+      dataType: "json",
+      method: "get",
+
+      success: function(data) {
+        $('#gp_field_division').empty();
+        $('#gp_field_division').append($("<option></option>").attr("value", null).text(""))
+        $.each(data.division, function(i, v) {
+          $('#gp_field_division').append($("<option></option>").attr("value", v.id).text(v.name));
+        });
+
+        $('#gp_field_branch').empty();
+        $('#gp_field_branch').append($("<option></option>").attr("value", null).text(""))
+        $.each(data.branch, function(i, v) {
+          $('#gp_field_branch').append($("<option></option>").attr("value", v.id).text(v.name));
+        });
+
+        $('#gp_field_unit').empty();
+        $('#gp_field_unit').append($("<option></option>").attr("value", null).text(""))
+        $.each(data.unit, function(i, v) {
+          $('#gp_field_unit').append($("<option></option>").attr("value", v.id).text(v.name));
+        });
+
+        $('#gp_field_org_code').empty();
+        $('#gp_field_org_code').append($("<option></option>").attr("value", null).text(""))
+        $.each(data.org_code, function(i, v) {
+          $('#gp_field_org_code').append($("<option></option>").attr("value", v.id).text(v.code));
+        });
+      },
+
+      error: function(xhdr, text, thrown) {
+        alert(thrown);
+      }
+    });
+  });
+
+  $('#gp_field_rotation').click(function() {
+    if(this.checked) {
+      $('#gp_org_code_2').removeClass('hide-element');
+    } else {
+      $('#gp_org_code_2').addClass('hide-element');
     }
   });
 });
