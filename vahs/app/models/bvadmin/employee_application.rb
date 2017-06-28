1239f2d258295f2209278368ba36e8b0d85fff88
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-06-28 10:03:02 -0400

    Employee Application / Employee Applicant model.

--- vahs/app/models/bvadmin/employee_application.rb
+++ vahs/app/models/bvadmin/employee_application.rb
@@ -1,4 +1,5 @@
 class Bvadmin::EmployeeApplication < Bvadmin::Record
   self.table_name = "BVADMIN.EMPLOYEE_APPLICATIONS"
   self.primary_key = :application_id
+  belongs_to :employee_applicants
 end
