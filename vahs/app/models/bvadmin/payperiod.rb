e75b6698aac25f0e9c79a8f2073d4240316c5b42
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-06-29 08:45:53 -0400

    Just a syntax change.

--- vahs/app/models/bvadmin/payperiod.rb
+++ vahs/app/models/bvadmin/payperiod.rb
@@ -1,12 +1,13 @@
 class Bvadmin::Payperiod < Bvadmin::Record
   self.table_name = "BVADMIN.PAYPERIODS"
 
+  # get current payperiod
+  scope :cur_pp, -> {
+    where("startdate <= :cur_date and enddate >= :cur_date", cur_date: Date.today)
+  }  
 
-#get current payperiod
-scope :cur_pp, -> {where("startdate <= :cur_date and enddate >= :cur_date", {cur_date: Date.today} )}  
-
-#get next payperiod
-scope :next_pp, -> {where("startdate <= :next_date and enddate >= :next_date", {next_date: Date.today+2.weeks} )}  
-
+  # get next payperiod
+  scope :next_pp, -> {
+    where("startdate <= :next_date and enddate >= :next_date", next_date: Date.today + 2.weeks)
+  }
 end
-
