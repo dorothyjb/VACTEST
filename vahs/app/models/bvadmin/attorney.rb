ba7d67c019e7e97aead0f64e779a142e2a889b9f
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-07 12:54:35 -0400

    Validate attorney_id presence.

--- vahs/app/models/bvadmin/attorney.rb
+++ vahs/app/models/bvadmin/attorney.rb
@@ -2,6 +2,8 @@ class Bvadmin::Attorney < Bvadmin::Record
   self.table_name = "BVADMIN.ATTORNEY"
   self.primary_key = :attorney_id
 
+  validates :attorney_id, presence: true
+
   belongs_to :employee
 
   def board_appt_date= date
