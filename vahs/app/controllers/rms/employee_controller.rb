3777ef04dac51daee36a8d398d88570fc9506c05
Ruth Spradlin <r.spradlin@cgifederal.com>
2017-07-07 10:37:24 -0400

    vahs/app/controllers/rms/employee_controller.rb

--- vahs/app/controllers/rms/employee_controller.rb
+++ vahs/app/controllers/rms/employee_controller.rb
@@ -115,6 +115,14 @@ class Rms::EmployeeController < Rms::ApplicationController
 
   def verify_access
     raise Rms::Error::UserAuthenticationError, "#{current_user.name} does not have access to this resource." unless current_user.has_role? :admin, :employee
+
+    #adding placeholder here to check to see if the current user has priveledges to view private information
+    #this will eventually pull from the roles tables, but for now, just setting a constant variable
+    unless current_user.has_role? :admin
+       @view_private_info = false
+    else
+       @view_private_info = true
+    end
   end
 
   def attorney_params
