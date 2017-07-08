c10766c0562be2f0c5eb2c9be752add527b22445
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-07 22:28:31 -0400

    use db/seeds/environment/*.rb for seeding.

--- vahs/db/seeds.rb
+++ vahs/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dir["#{Rails.root}/db/seeds/#{Rails.env.downcase}/*.rb"].each do |file|
+  begin
+    load file
+  rescue LoadError
+  end
+end
