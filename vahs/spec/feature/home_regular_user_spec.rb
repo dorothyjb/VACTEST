86dbde46aa7927c8205522c31945e542a1c82bed
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-07 22:31:08 -0400

    regular user spec.

--- vahs/spec/feature/home_regular_user_spec.rb
+++ vahs/spec/feature/home_regular_user_spec.rb
@@ -1,8 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Home Page" do
-  it "displays Index" do
-    visit "/"
-    expect(find("#content")).to have_content("Index")
+RSpec.feature 'Home Page as regular user' do
+  before(:each) do
+    visit '/'
+  end
+
+  it 'should contain Research & Analysis' do
+    expect(find('#content')).to have_content('Research & Analysis')
+  end
+
+  it 'should contain a link to the Employee Locator' do
+    expect(find('#content')).to have_link 'Employee Locator'
+  end
+
+  it 'should not contain Data Entry & Maintenance' do
+    expect(find('#content')).to_not have_content('Data Entry & Maintenance')
+  end
+
+  it 'should not contain Administrative Tools' do
+    expect(find('#content')).to_not have_content('Administrative Tools')
   end
 end
