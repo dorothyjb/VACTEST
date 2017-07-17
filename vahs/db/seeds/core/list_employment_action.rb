718548ea7a57d231db6aab2f00742497cd744308
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-17 09:36:13 -0400

    Seed data for RmsDropDownConfig.

---
+++ vahs/db/seeds/core/list_employment_action.rb
@@ -0,0 +1,7 @@
+[ 'Appointment', 'Seperaton', 'Promotion' ].each do |emp_action|
+  Bvadmin::RmsDropDownConfig.create(table_id: 'EMPLOYEE',
+                                    field_id: 'EMPLOYMENT_ACTION',
+                                    content: emp_action,
+                                    value: emp_action,
+                                    created_by: 'root@rms.config')
+end
