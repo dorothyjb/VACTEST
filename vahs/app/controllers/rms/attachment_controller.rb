85a95e678741863fd6ffc8a8917d0f5c06843c5f
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-19 16:04:00 -0400

    Attachments controller. Mostly just a proxy controller for the employee records tab.

--- vahs/app/controllers/rms/attachment_controller.rb
+++ vahs/app/controllers/rms/attachment_controller.rb
@@ -1,11 +1,44 @@
 class Rms::AttachmentController < Rms::ApplicationController
   before_filter :verify_access
 
+  def edit
+    @attachment = Bvadmin::RmsAttachment.find(params[:id])
+
+    respond_to do |format|
+      format.html { render partial: '/rms/employee/attachment/edit' }
+      format.js { render 'rms/attachment/edit' }
+    end
+
+  rescue Exception
+    flash[:error] = { "#{params[:id]}": "Attachment does not exist." }
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'rms/employee/errors' }
+    end
+  end
+
+  def delete
+    attachment = Bvadmin::RmsAttachment.find(params[:id])
+    attachment.destroy
+
+    respond_to do |format|
+      flash[:notice] = 'Attachment removed successfully'
+      format.js { render 'rms/attachment/delete' }
+    end
+
+  rescue Exception
+    flash[:error] = { "#{params[:id]}": "File does not exist" }
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'rms/attachment/delete_error' }
+    end
+  end
+
   def download
     attachment = Bvadmin::RmsAttachment.find(params[:id])
     send_data attachment.filedata, filename: attachment.filename,
                                    type: attachment.filetype,
-                                   disposition: 'attachment'
+                                   disposition: 'inline'
 
   rescue Exception
     flash[:error] = { "#{params[:id]}": "File does not exist" }
