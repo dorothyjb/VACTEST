7769cfda6b133567a2c84ad405b3f228558f1ccb
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-07-19 16:00:45 -0400

    Remove aria-labelledby / aria-describedby from intrusive javascript.

--- vahs/app/assets/javascripts/508ARIA.js
+++ vahs/app/assets/javascripts/508ARIA.js
@@ -1,34 +1,34 @@
 
 $( document ).ready(function() {
-	/* 508 Append ARIA tags using the 'alt' attribute */
+	/* 508 Append ARIA label tags using the 'alt' attribute */
 
 	$.each($('a'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 
 	$.each($('select'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 
 	$.each($('label'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 
 	$.each($('input'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 
 	$.each($('button'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 
 	$.each($('img'), function(k,obj){
 		var alt = $(obj).attr('alt');
-		$(obj).attr('title',alt).attr('aria-label',alt).attr('aria-describedby',alt).attr('aria-labelledby',alt);
+		$(obj).attr('title',alt).attr('aria-label',alt);
 	});
 });
