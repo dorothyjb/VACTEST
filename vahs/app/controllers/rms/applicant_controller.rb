a119c419b650a370acacd421109ba3eb8d8f82c6
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-08-03 17:14:41 -0400

    Applicant controller; support searching and copying an employee to an applicant.

--- vahs/app/controllers/rms/applicant_controller.rb
+++ vahs/app/controllers/rms/applicant_controller.rb
@@ -1,27 +1,80 @@
 class Rms::ApplicantController < Rms::ApplicationController
-  before_filter :verify_access, except: [ :locator ]
-  before_filter :check_for_cancel, only: [ :create ]
+  before_filter :verify_access
+
+  def index
+    @applicant = Bvadmin::EmployeeApplicant.new
+    @applicants = Bvadmin::PotentialApplicant.newsearch(params.dig(:applicant, :fname),
+                                                        params.dig(:applicant, :lname)).
+                                                        order('FNAME ASC').
+                                                        paginate(per_page: 10, page: params[:page])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 
   def new
     @applicant = Bvadmin::EmployeeApplicant.new
     @application = Bvadmin::EmployeeApplication.new
   end
 
+  def edit
+    @applicant = Bvadmin::EmployeeApplicant.find(params[:id])
+
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = { 'Applicant': 'Invalid ID' }
+    redirect_to rms_applicant_path
+  end
+
   def create
-    @applicant = Bvadmin::EmployeeApplicant.new
+    @applicant = Bvadmin::EmployeeApplicant.new(params.require(:applicant).
+                                                permit(:fname, :lname))
 
-    @applicant.update applicant_params
-    unless @applicant.valid?
+    if @applicant.valid?
+      @applicant.save
+
+      flash[:notice] = "Applicant #{@applicant.lname}, #{@applicant.fname} created."
+      redirect_to rms_applicant_edit_path(@applicant)
+    else
       flash[:error] = @applicant.errors
-      return render('rms/applicant/new')
+
+      @applicants = Bvadmin::PotentialApplicant.newsearch(params.dig(:applicant, :fname),
+                                                          params.dig(:applicant, :lname)).
+                                                          order('FNAME ASC').
+                                                          paginate(per_page: 10, page: params[:page])
+
+      render 'rms/applicant/index'
     end
-
-    @applicant.save
   end
 
-  def locator
+  def new_search
+    applicant = Bvadmin::PotentialApplicant.newsearch(params.dig(:applicant, :fname),
+                                                      params.dig(:applicant, :lname))
+    render json: {
+      'last' => lambda { applicant.select(:lname).distinct.order('LNAME ASC').first(15).map(&:lname) },
+      'first' => lambda { applicant.select(:fname).distinct.order('FNAME ASC').first(15).map(&:fname) },
+    }.fetch(params[:searchtype], lambda { [] }).call
   end
 
+  def copy
+    employee = Bvadmin::Employee.find(params[:id])
+    applicant = Bvadmin::EmployeeApplicant.new(fname: employee.fname,
+                                               lname: employee.lname,
+                                               grade: employee.grade,
+                                               series: employee.job_code)
+
+    applicant.save
+
+    flash[:notice] = "Applicant #{applicant.lname}, #{applicant.fname} created."
+    redirect_to rms_applicant_edit_path(applicant)
+
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = { 'Invalid Employee': 'Could not create that employee as an applicant' }
+    redirect_to rms_applicant_path
+  end
+
+private
 
   def verify_access
     raise Rms::Error::UserAuthenticationError, "#{current_user.name} does not have access to this resource." unless current_user.has_role? :admin, :employee
