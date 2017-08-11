15b86215a3aa3c4aeb8bc3a95c4755b5e0baae19
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-08-11 16:49:14 -0400

    RmsOrgInformation; Base class object so that Office/Division/Unit can use the same reporting functions within a 'scope'.

--- vahs/app/models/bvadmin/rms_org_information.rb
+++ vahs/app/models/bvadmin/rms_org_information.rb
@@ -6,4 +6,49 @@ class Bvadmin::RmsOrgInformation < Bvadmin::Record
   def self.dropdown
     [ '' ] + where('name is not null').collect { |n| [ n[:name], n[:id] ] }
   end
+
+  def total_fte
+    Bvadmin::Employee.joins(:org_codes).where(<<-EOQ).sum(:fte).to_f
+      rms_org_code.#{type_id} = #{self.id} and
+      employee.employee_id = rms_org_code.employee_id
+    EOQ
+  end
+
+  def total_vacant
+    Bvadmin::RmsOrgCode.where("employee_id is null and #{type_id} = #{self.id}").count
+  end
+
+  def total_on_board
+    0
+  end
+
+  def total_on_board_other
+    0
+  end
+
+  def funded_positions
+    Bvadmin::RmsOrgCode.where("rotation = 0 and #{type_id} = #{self.id}").count
+  end
+
+  def unfunded_positions
+    Bvadmin::RmsOrgCode.where("rotation = 1 and #{type_id} = #{self.id}").count
+  end
+
+  def total_incoming
+    0
+  end
+
+  def total_departing
+    0
+  end
+
+private
+  def type_id
+    {
+      'Bvadmin::RmsOrgOffice' => 'office_id',
+      'Bvadmin::RmsOrgDivision' => 'division_id',
+      'Bvadmin::RmsOrgBranch' => 'branch_id',
+      'Bvadmin::RmsOrgUnit' => 'unit_id',
+    }.fetch(self.class.to_s, 'id')
+  end
 end
