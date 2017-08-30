ceb7b8f352e9b774d9ac1c2d5e946d88d80a6114
Joseph Kinsella <joseph.kinsella@cgifederal.com>
2017-08-30 12:05:13 -0400

    change route get '/rms/organiziation/(office|division|branch|unit|org_code)' to get '/rms/organization/dropdown'.

--- vahs/config/routes.rb
+++ vahs/config/routes.rb
@@ -99,12 +99,7 @@ Rails.application.routes.draw do
 
     scope :organization, as: :organization do
       get '/', to: 'organization#index'
-
-      get '/office(.:format)', to: 'organization#office'
-      get '/division(.:format)', to: 'organization#division'
-      get '/branch(.:format)', to: 'organization#branch'
-      get '/unit(.:format)', to: 'organization#unit'
-      get '/org_code(.:format)', to: 'organization#org_code'
+      get '/dropdown', to: 'organization#dropdown'
     end
   end
 
